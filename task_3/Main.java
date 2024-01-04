8d88b38f3f88ccc689101290d7ed68b73f05e74c
GrimHail <133262668+GrimHail@users.noreply.github.com>
2024-01-03 22:18:00 -0500

    Task_3
    This's the task3 solved.

---
+++ task_3/Main.java
@@ -0,0 +1,18 @@
+package session_1.task_3;
+
+public class Main {
+    public static void main(String[] args) {
+        MountainBike mountainBike = new MountainBike("HFP154", (byte) 2, true, "Negro", false, 5000000.0d);
+        Bike bike = new Bike("GHV675", (byte) 2, false, "Amarillo", true, 1400000.0d);
+        Car car = new Car("GJH295", (byte) 4, (short) 5, "Rojo", true, 25600000.0d);
+        LuxuryCar luxuryCar = new LuxuryCar("PLE073", (byte) 4, (short) 9, "Azul", true, 40000000.0d);
+        Motorcycle motorcycle = new Motorcycle("NDF683", (byte) 4, (short) 15, "Gris", true, 9400000.0d);
+
+        mountainBike.getInformation();
+        bike.getInformation();
+        car.getInformation();
+        luxuryCar.getInformation();
+        motorcycle.getInformation();
+
+    }
+}
